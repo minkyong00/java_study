3dd7f86ec3e27d65c8a616074647058231c04ec2
minkyong00 <minkyong131@gmail.com>
2025-07-01 00:49:58 +0900

    java network ExPubAPI2 공공API 입력한 역의 시간표 파일에 추가

--- src/pub2504/network/ExPubAPI2.java
+++ src/pub2504/network/ExPubAPI2.java
@@ -1,13 +1,126 @@
 package pub2504.network;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 /*
 
 [공공API 활용 실습2]
-http://apis.data.go.kr/1613000/SubwayInfoService/getSubwaySttnAcctoSchdulList?serviceKey=bafEXsp3A%2FiNM6SrdTReCSgGSp3PZcxqoiD08onBtPTnKgLGfaCfkXnJa15dbR8zVjWpmN99qG5QMrpT%2FjWbuQ%3D%3D&subwayStationId=MTRS11133&dailyTypeCode=01&upDownTypeCode=D&numOfRows=10&pageNo=1&_type=json
+http://apis.data.go.kr/1613000/SubwayInfoService/getSubwaySttnAcctoSchdulList
+?serviceKey=bafEXsp3A%2FiNM6SrdTReCSgGSp3PZcxqoiD08onBtPTnKgLGfaCfkXnJa15dbR8zVjWpmN99qG5QMrpT%2FjWbuQ%3D%3D
+&subwayStationId=MTRS11133
+&dailyTypeCode=01
+&upDownTypeCode=D
+&numOfRows=10
+&pageNo=1
+&_type=json
 역명 입력하면 해당 역의 시간표 출력
 역명_timetable.csv로 저장
 */
 
 public class ExPubAPI2 {
+	
+	private static String subwayCode = "";
+	private static String subwayName = "";
+
+	private static final String REQUEST_CODE_URL 
+	= "http://openapi.seoul.go.kr:8088/46644750596d696e3832594e516569/json/SearchInfoBySubwayNameService/1/5/";
+
+	private static final String REQUEST_URL 
+    = "http://openapi.seoul.go.kr:8088/64414f66516d696e37376853656444/json/SearchSTNTimeTableByIDService/1/";
+ 
+	private static final HttpClient client = HttpClient.newHttpClient();
+	
+	private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	
+	public static void main(String[] args) {
+		
+		PrintWriter pw = null;
+		
+		try {
+			
+			subwayCode = getSubwayCode("화랑대").get(0);
+			subwayName = getSubwayCode("화랑대").get(1);
+			
+			File file = new File("C:\\pub2504\\files\\" + subwayName + "_timetable.csv");
+			pw = new PrintWriter(new FileWriter(file));
+			List<String[]> data = getAPIData(getSubwayTimeCount());
+			pw.println("출발시간,도착시간,종착역");
+			System.out.println("출발시간,도착시간,종착역");
+			for(String[] line : data) {
+				pw.println(String.join(",", line));
+				System.out.println(String.join(",", line));
+			}
+			pw.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	// api jsonObject로 변환
+	private static JsonObject getJsonObect(String url, String api) throws Exception {
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create(url)).GET().build();
+		HttpResponse<String> response 
+			= client.send(request, HttpResponse.BodyHandlers.ofString());
+		JsonObject jsonObject = gson.fromJson(response.body(), JsonObject.class)
+				.getAsJsonObject(api);
+		return jsonObject;
+	} // getJsonObject
+	
+	// 열차 시간표 출력 메소드
+	private static List<String[]> getAPIData(String count) throws Exception {
+		String url = REQUEST_URL + count + "/" + subwayCode + "/1/1/";
+		JsonArray jsonArray = getJsonObect(url, "SearchSTNTimeTableByIDService")
+			.getAsJsonArray("row");
+		List<String[]> strList = new ArrayList<String[]>();
+		for(JsonElement e : jsonArray) {
+			JsonObject obj = e.getAsJsonObject();
+			strList.add(new String[] {
+				obj.get("ARRIVETIME").getAsString(),
+				obj.get("LEFTTIME").getAsString(),
+				obj.get("SUBWAYSNAME").getAsString()
+			});
+		}
+		return strList;
+   } // getAPIData  
+	
+	// 지하철역 입력하면 지하철 코드 출력
+	private static List<String> getSubwayCode(String subwayName) throws Exception {
+		String url = REQUEST_CODE_URL + subwayName;
+		JsonObject stationObj = getJsonObect(url, "SearchInfoBySubwayNameService")
+				.get("row").getAsJsonArray()
+				.get(0).getAsJsonObject();
+		List<String> strList = Arrays.asList(
+			stationObj.get("STATION_CD").getAsString(),
+			subwayName
+		);
+		return strList;
+	} // getSubwayCode
+
+	// 지하철역 시간표 전체 개수 출력
+	private static String getSubwayTimeCount() throws Exception {
+		// 164/2647/1/1/
+		String url = REQUEST_URL + "120/" + subwayCode + "/1/1";
+		String count = getJsonObect(url, "SearchSTNTimeTableByIDService")
+			.get("list_total_count").getAsString();
+		return count;
+	} // getSubwayTimeCount
 
 }
